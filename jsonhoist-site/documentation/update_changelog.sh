6f9c93f02e8c4e8612492265f0aa97fa2668b3c3
Uwe Schaefer <uwe.schaefer@mercateo.com>
2018-12-28 13:35:30 +0100

    cleanup

--- jsonhoist-site/documentation/update_changelog.sh
+++ jsonhoist-site/documentation/update_changelog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git tag -l | xargs git tag -d && git fetch -t
 rm -rf /tmp/github*
-github_changelog_generator -u Mercateo -p factcast -o content/changelog.md 
+github_changelog_generator -u uweschaeefer -p jsonhoist -o content/changelog.md 
